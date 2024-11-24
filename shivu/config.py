9b3e30a630b05db3c6a52f06f4ed75e7aa19b002
VILLAIN <iamrkay420@gmail.com>
2024-11-24 15:50:17 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -5,7 +5,7 @@ class Config(object):
     OWNER_ID = "5909658683"
     sudo_users = "5909658683", "8019277081", "5608779258", "6961368696", "1881562083"
     GROUP_ID = -1002311769574
-    TOKEN = "7655351916:AAE5evDq7VVWaW5Qx4lmWwRlCycjwHBtqe4"
+    TOKEN = "7655351916:AAFk4dBwLuylhN2cyfhynk8VpMw_7UJXK7M"
     mongo_url = "mongodb+srv://TEAMBABY01:UTTAMRATHORE09@cluster0.vmjl9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     PHOTO_URL = ["https://files.catbox.moe/wy70cl.jpg", "https://files.catbox.moe/wy70cl.jpg"]
     SUPPORT_CHAT = "iamvillain77"
